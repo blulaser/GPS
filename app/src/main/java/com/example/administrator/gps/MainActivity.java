3a8012dfa9ebb53b965eee6b51149a88a2547672
blulaser <blulaser@naver.com>
2017-07-15 22:02:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/administrator/gps/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.administrator.gps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Timer;
+
+import java.util.TimerTask;
+
+
+
+
+
+
+public class MainActivity extends Activity {
+
+
+
+    private Button btnShowLocation;
+    private Button serviceon;
+    private Button serviceoff;
+
+    private TextView txtLat;
+
+    private TextView txtLon;
+
+
+
+    // GPSTracker class
+
+    private GPS gps;
+
+
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        RequestPermission R1 = new RequestPermission();
+        R1.ReqPer(this);
+
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  }, 0 );
+        }
+
+
+        btnShowLocation = (Button) findViewById(R.id.btn_start);
+        serviceon = (Button)findViewById(R.id.Serviceon);
+        serviceon = (Button)findViewById(R.id.Serviceon);
+        txtLat = (TextView) findViewById(R.id.Latitude);
+        txtLon = (TextView) findViewById(R.id.Longitude);
+
+
+
+
+
+
+
+
+
+
+        // GPS 정보를 보여주기 위한 이벤트 클래스 등록
+
+
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+
+
+            public void onClick(View arg0) {
+
+                gps = new GPS(MainActivity.this);
+
+
+
+                if (gps.isGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    txtLat.setText(String.valueOf(latitude));
+                    txtLon.setText(String.valueOf(longitude));
+
+                    Toast.makeText(getApplicationContext(),"당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude, Toast.LENGTH_LONG).show();
+
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+
+
+                Intent intent = new Intent(
+                        getApplicationContext(),//현재제어권자
+                        Service2.class); // 이동할 컴포넌트
+                startService(intent); // 서비스 시작
+
+            }
+
+
+        });
+
+
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/administrator/gps/Service2.java
@@ -0,0 +1,128 @@
+package com.example.administrator.gps;
+
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class Service2 extends Service {
+    Location location;
+    protected LocationManager locationManager;
+
+    private GPS gps2;
+    private boolean isStop;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private  Context mContext;
+    double lat1;
+    double lon1;
+    Location locationA = new Location("point A");
+    double distance;
+    Location locationB = new Location("point B");
+
+    // 최소 GPS 정보 업데이트 시간 밀리세컨이므로 1분
+
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5 * 1;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Service 객체와 (화면단 Activity 사이에서)
+        // 통신(데이터를 주고받을) 때 사용하는 메서드
+        // 데이터를 전달할 필요가 없으면 return null;
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        isStop=false;
+        gps2 = new GPS(this);
+
+        mContext = this;
+        Log.d("test", "서비스의 onCreate");
+        Thread counter = new Thread(new Counter()); counter.start();
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+
+
+
+
+
+
+
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // 서비스가 호출될 때마다 실행
+        Log.d("test", "서비스의 onStartCommand");
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private class  Counter implements Runnable { private int count; private Handler handler = new Handler(); @Override public void run() { for (count = 0; count < 5; count++) { // STOP 버튼을 눌렀다면 종료한다.
+
+        if (isStop) { break; } /** * Thread 안에서는 UI와 관련된 Toast를 쓸 수 없습니다. * 따라서, Handler를 통해 이용할 수 있도록 만들어줍니다. */
+        handler.post(new Runnable() { @Override public void run() {
+            // Toast로 Count 띄우기
+
+
+                if(count==0) {
+                    lat1 = gps2.lat;
+                    lon1 = gps2.lon;
+                    locationA.setLatitude(lat1);
+                    locationA.setLongitude(lon1);
+                }
+
+
+
+            locationB.setLatitude(gps2.lat);
+            locationB.setLongitude(gps2.lon);
+
+            distance = locationA.distanceTo(locationB);
+                if(distance<300) {
+                    Toast.makeText(getApplicationContext(), distance + "m," + gps2.lat + "," + gps2.lon + " "+lat1+","+lon1, Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "#"+distance + "m," + gps2.lat + "," + gps2.lon + " "+lat1+","+lon1, Toast.LENGTH_SHORT).show();
+                   isStop=true;
+                }
+                if(count==4)
+                {
+                    //TODO 저장할건지 호출하는거 넣는위치
+                }
+
+            // Log로 Count 찍어보기
+            Log.d("COUNT,", count + ""); } });
+        // Sleep을 통해 1초씩 쉬도록 한다.
+        try { Thread.sleep(1000*30); } catch (InterruptedException e) { e.printStackTrace(); } } handler.post(new Runnable() { @Override public void run() { Toast.makeText(getApplicationContext(), "서비스가 종료되었습니다.", Toast.LENGTH_SHORT).show(); } }); } }
+
+
+
+
+    @Override
+    public void onDestroy() {
+
+
+        Log.d("test", "서비스의 onDestroy");
+        super.onDestroy();
+        isStop = true;
+        // 서비스가 종료될 때 실행
+    }
+
+
+
+
+
+
+
+
+
+
+}
